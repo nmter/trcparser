843b64224274a03f0013d491a9bfc6c400142a32
mnter <mcmggradty1991@gmail.com>
2015-12-14 17:29:36 +0800

    12-14

--- supplement/3feature_counter.py
+++ supplement/3feature_counter.py
@@ -15,16 +15,61 @@ trace_file
 	xxx xxx xxxx ....
 '''
 def do_default():
-	print 'do nothing'
+	print "it\'s an error here."
 
-def do_offset(xdict, words, ofs_band=1):
-	#default 1M-width.
-	#default no time-consideration.
+def insert_dict(xdict, key):
+	if(not xdict.has_key(key)):
+		xdict[key] = 1
+	else:
+		xdict[key] += 1
+
+def do_offset_adjust_ofs_band():
+	'''this func not complemented.
+	'''
+	return 0
+
+
+def display_dict_in_tables(xdict):
+	#print 'xxxxx'
+	#print xdict.items() 
+	#sorted(xdict.items(), lambda x,y:cmp(x[1],y[1]))
+	#print 'tttttt'
+	#print xdict.items() 
+	return 
+	
+def display_dict(xdict, way):
+	if(way == 'table'):
+		display_dict_in_tables(xdict)
+
+
+def do_offset(xdict_list, words, ofs_band=1):
+	'''{ofs_band} MBytes-width.
+		default no time-consideration.
+		
+		@param xdict_list: The feature dict list which is defined outside.
+		The elements of xdict_list:
+		xdict_list[0]: dict for read
+		xdict_list[1]: dict for write
+		xdict_list[2]: dict for all
+		
+		@param words: the line read from trace file.
+		
+		@param ofs_band=1: the width of offset.
+	'''
+	
+	
+	#this req.'s time(in sec.)
 	time = words[0]
+	
+	#this req.'s offset(in {ofs_band}MB.)
 	ofs = words[2]
-	
-	
-	print time, ofs
+	ofs = int(ofs) / ( ofs_band * 1024 * 2 )
+	if(words[1] == 'R'):
+		insert_dict(xdict_list[0], ofs)
+	else:
+		insert_dict(xdict_list[1], ofs)
+	insert_dict(xdict_list[2], ofs)
+
 
 def do_size(xdict, words):
 	print 'here is do_size'
@@ -33,9 +78,14 @@ def do_IOPS(xdict, words):
 	print 'here is do_IOPS'
 	
 def generate_dicts():
+	f_idx = 0
 	for which in ['offset','size','IOPS']:
-		#the feature dict.
-		feature_dict = dict();
+		
+		
+		#3 feature_dict_lists for offset/size/IOPS
+		feature_dict_lists = [[{}, {}, {}],	#lifetime ?
+		[{}, {}, {}],
+		[{}, {}, {}]]
 		
 		#3 features' function
 		whichs = {
@@ -49,8 +99,12 @@ def generate_dicts():
 		for l in fp.readlines():
 			words = l.split()
 			if(len(words) == 7):
-				whichs.get(which, do_default)(feature_dict, words)
+				whichs.get(which, do_default)(feature_dict_lists[f_idx], words)
 		fp.close()
-
+		f_idx += 1
+	return 
 if __name__ == '__main__':
 	generate_dicts()
+	for i in xdict_list:
+		print len(xdict_list)
+		display_dict(i, 'table')
