25264d097f4ad38cf15e9afdd350b6ba9a707484
mnter <mcmggradty1991@gmail.com>
2016-03-22 14:47:30 +0800

    3.22

--- dispatcher/my_RMeter.cpp
+++ dispatcher/my_RMeter.cpp
@@ -3,25 +3,43 @@
 #include <stdlib.h>
 #include "my_RMeter.h"
 
+
+
 global_args G_args;
+static const char *opt_str = "t:o:d:h?";//static & const.
+
 
 void usage(void)
 {
- 	printf("usage:%s [-t tracefilename] [-r resultfilename] [-p processnumber] [-m time] [-nnumber] dev\n",myname);
-	printf("      [-t tracefilename]:\n");
-	printf("      [-r resultfilename]:\n");
-	printf("      [-c raid capacity(KB)]:\n");
-	printf("      [-i timescale(default:1, faster 2+)]:\n");
-	printf("      [-a rangescale(default:0, bigger 2+)]:\n");
-	printf("      [-m deal time]:\n");
-	printf("      [-n deal number]:\n");
-	printf("      dev:\n");
+	printf("fuck RMeter @ywj\n");
+	printf("usage: %s -t trace_file_name [-o result_file_name] -d device_name\n", G_args.executable_name);
 }
 
+void do_args(int argc, char* argv[])//[this version] the args don't support long format.
+{
+	int opt = 0;
+	initialze_global_args(&G_args, argv);
+	while((opt = getopt(argc, argv, opt_str))!= -1){
+		switch(opt){
+			case 't'://-t trace_file_name
+				G_args.trace_file_name = optarg;break;
+			case 'o'://-o result_file_name
+				G_args.result_file_name = optarg;break;
+			case 'd'://-d device_name
+				G_args.dev_name = optarg;break;
+			case 'h'://-h
+			case '?'://error will get here.
+				usage();break;
+			default :
+				break;
+		}
+	}
+	
+}
 
 
 int main(int argc, char* argv[])
 {
-	
+	do_args(argc, argv);
 	return 0;
 }
--- dispatcher/my_RMeter.h
+++ dispatcher/my_RMeter.h
@@ -1,8 +1,15 @@
 
 typedef struct global_args{
-	char trace_file_name[255];
-	char result_file_name[255];
-	char executable_name[255];
-	
-	char dev_name[255];
+	const char* trace_file_name;
+	const char* result_file_name;
+	const char* executable_name;
+	const char* dev_name;
 }global_args;
+
+#define EXIT_FAILURE -1
+#define EXIT_SUCCESS 0
+
+void initialze_global_args(global_args *g, char* argv[]){
+	g->executable_name = argv[0];
+	g->result_file_name = g->dev_name = g->trace_file_name = NULL;
+}
